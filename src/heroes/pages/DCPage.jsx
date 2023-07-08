6ae641c4107f0c2ce596f67a282a75d17b737838
Ryuma <rjnl85@gmail.com>
2023-07-08 14:50:22 -0500

    Add a hero list to MarvelPage and DCPage

--- src/heroes/pages/DCPage.jsx
+++ src/heroes/pages/DCPage.jsx
@@ -1,6 +1,11 @@
+import { HeroList } from "../components"
 
 export const DCPage = () => {
   return (
-    <h1>DCPage</h1>
+    <>
+      <h1>DC Comics</h1>
+      <hr />
+      <HeroList publisher={'DC Comics'} />
+    </>
   )
 }
