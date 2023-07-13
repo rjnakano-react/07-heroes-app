50029904c917d2171988b4c0066d6aa32123ea0c
Ryuma Nakano <ryuma@nakasan.co>
2023-07-13 11:48:48 -0500

    Prevent the user to access LoginPage while still logged on

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 
+import { PrivateRoute, PublicRoute } from '.'
 import { LoginPage } from '../auth'
 import { HeroesRoutes } from '../heroes'
 
@@ -10,9 +11,26 @@ export const AppRouter = () => {
       <div className='container'>
         <Routes>
 
-          <Route path='/*' element={<HeroesRoutes />} />
-          <Route path='/login' element={<LoginPage />} />
+          {/* <Route path='/*' element={<HeroesRoutes />} /> */}
+          <Route path='/*' element={
+            <PrivateRoute>
+              <HeroesRoutes />
+            </PrivateRoute>
+          } />
 
+          {/* <Route path='/login' element={<LoginPage />} /> */}
+          {/* <Route path='/login' element={
+            <PublicRoute>
+              <LoginPage />
+            </PublicRoute>
+          } /> */}
+          <Route path='/login/*' element={
+            <PublicRoute>
+              <Routes>
+                <Route path='/*' element={<LoginPage />} />
+              </Routes>
+            </PublicRoute>
+          } />
 
         </Routes>
 
