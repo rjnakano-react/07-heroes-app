78ef9a98095838292763ebecd332eb8fddb1a9b4
Ryuma <rjnl85@gmail.com>
2023-07-13 19:12:25 -0500

    Test types.js

---
+++ __tests__/auth/types/types.test.js
@@ -0,0 +1,10 @@
+import { types } from "../../../src/auth/types/types"
+
+describe('Test types.js', () => { 
+    test('should return the following types', () => { 
+        expect(types).toEqual({
+            login: '[Auth] Login',
+            logout: '[Auth] Logout'
+        })
+     })
+ })
